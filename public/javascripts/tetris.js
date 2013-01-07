e20289378ea14e19d86f6823fe51eebf165161e6
jeremy <cjuhong@gmail.com>
2013-01-08 07:48:32 +0800

    add blocks

--- public/javascripts/tetris.js
+++ public/javascripts/tetris.js
@@ -1,4 +1,17 @@
+if ( !window.requestAnimationFrame ) {
+  window.requestAnimationFrame = ( function() {
+    return window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+	window.setTimeout( callback, 1000 / 60 );
+      };
+  })();
+}
+
 var Tetris = {};
+
 Tetris.init = function() {
   // set the scene size
   var WIDTH = window.innerWidth,
@@ -14,9 +27,9 @@ Tetris.init = function() {
   // and a scene
   Tetris.renderer = new THREE.WebGLRenderer();
   Tetris.camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
-                                                ASPECT,
-                                                NEAR,
-                                                FAR  );
+	                                        ASPECT,
+	                                        NEAR,
+	                                        FAR  );
   Tetris.scene = new THREE.Scene();
 
   // the camera starts at 0,0,0 so pull it back
@@ -28,8 +41,7 @@ Tetris.init = function() {
 
   // attach the render-supplied DOM element
   document.body.appendChild(Tetris.renderer.domElement);
-
-  // to be continued...
+  
   // configuration object
   var boundingBoxConfig = {
     width: 360,
@@ -39,21 +51,17 @@ Tetris.init = function() {
     splitY: 6,
     splitZ: 20
   };
-
   Tetris.boundingBoxConfig = boundingBoxConfig;
   Tetris.blockSize = boundingBoxConfig.width/boundingBoxConfig.splitX;
 
   var boundingBox = new THREE.Mesh(
-    new THREE.CubeGeometry(
-      boundingBoxConfig.width, boundingBoxConfig.height, boundingBoxConfig.depth,
-      boundingBoxConfig.splitX, boundingBoxConfig.splitY, boundingBoxConfig.splitZ),
+    new THREE.CubeGeometry(boundingBoxConfig.width, boundingBoxConfig.height, boundingBoxConfig.depth, boundingBoxConfig.splitX, boundingBoxConfig.splitY, boundingBoxConfig.splitZ), 
     new THREE.MeshBasicMaterial( { color: 0xffaa00, wireframe: true } )
   );
   Tetris.scene.add(boundingBox);
 
-  // first render
   Tetris.renderer.render(Tetris.scene, Tetris.camera);
-  // to be continued...
+
   Tetris.stats = new Stats();
   Tetris.stats.domElement.style.position = 'absolute';
   Tetris.stats.domElement.style.top = '10px';
@@ -67,24 +75,13 @@ Tetris.init = function() {
 };
 
 Tetris.start = function() {
-   document.getElementById("menu").style.display = "none";
-   Tetris.pointsDOM = document.getElementById("points");
-   Tetris.pointsDOM.style.display = "block";
+  document.getElementById("menu").style.display = "none";
+  Tetris.pointsDOM = document.getElementById("points");
+  Tetris.pointsDOM.style.display = "block";
 
-   Tetris.animate();
+  Tetris.animate();
 };
 
-if ( !window.requestAnimationFrame ) {
-  window.requestAnimationFrame = ( function() {
-    return window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
-      window.setTimeout( callback, 1000 / 60 );
-    };
-  })();
-}
 Tetris.gameStepTime = 1000;
 
 Tetris.frameTime = 0; // ms
@@ -92,6 +89,7 @@ Tetris.cumulatedFrameTime = 0; // ms
 Tetris._lastFrameTime = Date.now(); // timestamp
 
 Tetris.gameOver = false;
+
 Tetris.animate = function() {
   var time = Date.now();
   Tetris.frameTime = time - Tetris._lastFrameTime;
@@ -109,5 +107,40 @@ Tetris.animate = function() {
 
   if(!Tetris.gameOver) window.requestAnimationFrame(Tetris.animate);
 }
+
+
+
+// nice test:
+// var i = 0, j = 0, k = 0, interval = setInterval(function() {if(i==6) {i=0;j++;} if(j==6) {j=0;k++;} if(k==6) {clearInterval(interval); return;} Tetris.addStaticBlock(i,j,k); i++;},30)
+
+Tetris.staticBlocks = [];
+Tetris.zColors = [
+  0x6666ff, 0x66ffff, 0xcc68EE, 0x666633, 0x66ff66, 0x9966ff, 0x00ff66, 0x66EE33, 0x003399, 0x330099, 0xFFA500, 0x99ff00, 0xee1289, 0x71C671, 0x00BFFF, 0x666633, 0x669966, 0x9966ff
+];
+Tetris.addStaticBlock = function(x,y,z) {
+  if(Tetris.staticBlocks[x] === undefined) Tetris.staticBlocks[x] = [];
+  if(Tetris.staticBlocks[x][y] === undefined) Tetris.staticBlocks[x][y] = [];
+
+  var mesh = THREE.SceneUtils.createMultiMaterialObject(new THREE.CubeGeometry( Tetris.blockSize, Tetris.blockSize, Tetris.blockSize), [
+    new THREE.MeshBasicMaterial({color: 0x000000, shading: THREE.FlatShading, wireframe: true, transparent: true}),
+    new THREE.MeshBasicMaterial({color: Tetris.zColors[z]}) 
+  ] );
+
+  mesh.position.x = (x - Tetris.boundingBoxConfig.splitX/2)*Tetris.blockSize + Tetris.blockSize/2;
+  mesh.position.y = (y - Tetris.boundingBoxConfig.splitY/2)*Tetris.blockSize + Tetris.blockSize/2;
+  mesh.position.z = (z - Tetris.boundingBoxConfig.splitZ/2)*Tetris.blockSize + Tetris.blockSize/2;
+
+  Tetris.scene.add(mesh);
+  Tetris.staticBlocks[x][y][z] = mesh;
+};
+
+Tetris.currentPoints = 0;
+Tetris.addPoints = function(n) {
+  Tetris.currentPoints += n;
+  Tetris.pointsDOM.innerHTML = Tetris.currentPoints;
+  Cufon.replace('#points');
+}
+
+//var i = 0, j = 0, k = 0, interval = setInterval(function() {if(i==6) {i=0;j++;} if(j==6) {j=0;k++;} if(k==6) {clearInterval(interval); return;} Tetris.addStaticBlock(i,j,k); i++;},30);
 
 window.addEventListener("load", Tetris.init);
