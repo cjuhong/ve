105a2dd71b20fdd587638554e63f91a8ff9c14e1
jeremy <cjuhong@gamil.com>
2013-01-09 20:32:42 +0800

    move and rotate

--- public/javascripts/tetris.block.js
+++ public/javascripts/tetris.block.js
@@ -1,11 +1,9 @@
-window.Tetris = window.Tetris  || {};
-
+window.Tetris = window.Tetris  || {}; // equivalent to if(!window.Tetris) window.Tetris = {};
 Tetris.Utils = {};
 
 Tetris.Utils.cloneVector = function (v) {
   return {x: v.x, y: v.y, z: v.z};
 };
-
 Tetris.Block = {};
 
 Tetris.Block.shapes = [
@@ -45,14 +43,15 @@ Tetris.Block.position = {};
 Tetris.Block.generate = function() {
   var geometry, tmpGeometry;
 
-  var type = Math.floor(Math.random()*(Tetris.Block.shapes.length));
+  var type = 1;
+  //  var type = Math.floor(Math.random()*(Tetris.Block.shapes.length));
   this.blockType = type;
 
   Tetris.Block.shape = [];
   for(var i = 0; i < Tetris.Block.shapes[type].length; i++) {
     Tetris.Block.shape[i] = Tetris.Utils.cloneVector(Tetris.Block.shapes[type][i]);
   }
-
+  // to be continued...
   geometry = new THREE.CubeGeometry(Tetris.blockSize, Tetris.blockSize, Tetris.blockSize);
   for(var i = 1 ; i < Tetris.Block.shape.length; i++) {
     tmpGeometry = new THREE.Mesh(new THREE.CubeGeometry(Tetris.blockSize, Tetris.blockSize, Tetris.blockSize));
@@ -60,15 +59,14 @@ Tetris.Block.generate = function() {
     tmpGeometry.position.y = Tetris.blockSize * Tetris.Block.shape[i].y;
     THREE.GeometryUtils.merge(geometry, tmpGeometry);
   }
-
+  // to be continued...
   Tetris.Block.mesh = THREE.SceneUtils.createMultiMaterialObject(geometry, [
     new THREE.MeshBasicMaterial({color: 0x000000, shading: THREE.FlatShading, wireframe: true, transparent: true}),
     new THREE.MeshBasicMaterial({color: 0xff0000})
   ]);
-
+  // to be continued...
   // initial position
   Tetris.Block.position = {x: Math.floor(Tetris.boundingBoxConfig.splitX/2)-1, y: Math.floor(Tetris.boundingBoxConfig.splitY/2)-1, z: 15};
-  Tetris.Block.rotation = {x: 0, y: 0, z: 0};
 
   Tetris.Block.mesh.position.x = (Tetris.Block.position.x - Tetris.boundingBoxConfig.splitX/2)*Tetris.blockSize/2;
   Tetris.Block.mesh.position.y = (Tetris.Block.position.y - Tetris.boundingBoxConfig.splitY/2)*Tetris.blockSize/2;
@@ -76,116 +74,85 @@ Tetris.Block.generate = function() {
   Tetris.Block.mesh.rotation = {x: 0, y: 0, z: 0};
   Tetris.Block.mesh.overdraw = true;
 
-
-  var collision = Tetris.Board.moveBlock(Tetris.Block);
-
-  if(collision == Tetris.Board.COLLISION_GROUND) {
-    Tetris.gameOver = true;
-    Tetris.sounds["gameover"].play();
-    Tetris.pointsDOM.innerHTML = "GAME OVER";
-    Cufon.replace('#points');
-  }
-
   Tetris.scene.add(Tetris.Block.mesh);
-};
-
-Tetris.Block.stepForward = function() {
-  Tetris.Block.mesh.position.z -= Tetris.blockSize;
-  Tetris.Block.position.z -= 1;
-
-  var collision = Tetris.Board.moveBlock(Tetris.Block);
-
-  if(collision == Tetris.Board.COLLISION_GROUND) {
-    Tetris.sounds["collision"].play();
-    Tetris.Block.position.z += 1;
-    Tetris.Block.hitBottom();
-
-    if(Tetris.Board.checkCompleted()) {
-      var fields = Tetris.Board.fields;
-
-      for(var z = 0; z < fields[0][0].length-1; z++) {
-	for(var y = 0; y < fields[0].length; y++) {
-	  for(var x = 0; x < fields.length; x++) {
-	    if(fields[x][y][z] == 1 && !Tetris.staticBlocks[x][y][z]) {
-	      Tetris.addStaticBlock(x,y,z);
-	    }
-	    if(fields[x][y][z] == 0 && Tetris.staticBlocks[x][y][z]) {
-	      Tetris.scene.removeObject(Tetris.staticBlocks[x][y][z]);
-	      Tetris.staticBlocks[x][y][z] = undefined;
-	    }
-	  }
-	}
-      }
-    }
-  } else {
-    Tetris.sounds["move"].play();
-  }
-  // return hit the bottom
-};
-
-
+}; // end of Tetris.Block.generate()
 Tetris.Block.rotate = function(x,y,z) {
   Tetris.Block.mesh.rotation.x += x * Math.PI / 180;
   Tetris.Block.mesh.rotation.y += y * Math.PI / 180;
   Tetris.Block.mesh.rotation.z += z * Math.PI / 180;
-
-
-  var rotationMatrix = new THREE.Matrix4();
-  rotationMatrix.setRotationFromEuler(Tetris.Block.mesh.rotation);
-  rotationMatrix.round();
-
-  for(var i = 0 ; i < Tetris.Block.shape.length; i++) {
-    Tetris.Block.shape[i] = rotationMatrix.multiplyVector3(
-      Tetris.Utils.cloneVector(Tetris.Block.shapes[this.blockType][i])
-    );
-  }
-
-  var collision = Tetris.Board.moveBlock(Tetris.Block);
-  if(collision == Tetris.Board.COLLISION_WALL) {
-    Tetris.Block.rotate(-x,-y,-z); //oh laziness
-  }
 };
-
 Tetris.Block.move = function(x,y,z) {
-  if(x) {
-    Tetris.Block.mesh.position.x += x;
-    Tetris.Block.position.x += x>0?1:-1;
-  }
-  if(y) {
-    Tetris.Block.mesh.position.y += y;
-    Tetris.Block.position.y += y>0?1:-1;
-  }
-  if(z) {
-    Tetris.Block.stepForward();
-  }
+  Tetris.Block.mesh.position.x += x*Tetris.blockSize;
+  Tetris.Block.position.x += x;
 
-  var collision = Tetris.Board.moveBlock(Tetris.Block);
-  if(collision == Tetris.Board.COLLISION_WALL) {
-    if(x) {
-      Tetris.Block.mesh.position.x -= x;
-      Tetris.Block.position.x -= x>0?1:-1;
-    }
-    if(y) {
-      Tetris.Block.mesh.position.y -= y;
-      Tetris.Block.position.y -= y>0?1:-1;
-    }
-  }
+  Tetris.Block.mesh.position.y += y*Tetris.blockSize;
+  Tetris.Block.position.y += y;
+
+  Tetris.Block.mesh.position.z += z*Tetris.blockSize;
+  Tetris.Block.position.z += z;
+  if(Tetris.Block.position.z == 0) Tetris.Block.hitBottom();
 };
-
-/**
- * call when hits the floor and should be transformed to static blocks
- */
+Tetris.Block.hitBottom = function() {
+  Tetris.Block.petrify();
+  Tetris.scene.removeObject(Tetris.Block.mesh);
+  Tetris.Block.generate();
+};
 Tetris.Block.petrify = function() {
   var shape = Tetris.Block.shape;
   for(var i = 0 ; i < shape.length; i++) {
     Tetris.addStaticBlock(Tetris.Block.position.x + shape[i].x, Tetris.Block.position.y + shape[i].y, Tetris.Block.position.z + shape[i].z);
-    Tetris.Board.fields[Tetris.Block.position.x + shape[i].x][Tetris.Block.position.y + shape[i].y][Tetris.Block.position.z + shape[i].z] = 1;
   }
 };
-
-Tetris.Block.hitBottom = function() {
-  this.petrify();
-
-  Tetris.scene.removeObject(Tetris.Block.mesh);
-  Tetris.Block.generate();
+Tetris.start = function() {
+  document.getElementById("menu").style.display = "none";
+  Tetris.pointsDOM = document.getElementById("points");
+  Tetris.pointsDOM.style.display = "block";
+  Tetris.Block.generate(); // add this line
+  Tetris.animate();
 };
+while(Tetris.cumulatedFrameTime > Tetris.gameStepTime) {
+   Tetris.cumulatedFrameTime -= Tetris.gameStepTime;
+   Tetris.Block.move(0,0,-1); // add this line
+}
+window.addEventListener('keydown', function (event) {
+  var key = event.which ? event.which : event.keyCode;
+
+  switch(key) {
+    case 38: // up (arrow)
+      Tetris.Block.move(0, 1, 0);
+      break;
+    case 40: // down (arrow)
+      Tetris.Block.move(0, -1, 0);
+      break;
+    case 37: // left(arrow)
+      Tetris.Block.move(-1, 0, 0);
+      break;
+    case 39: // right (arrow)
+      Tetris.Block.move(1, 0, 0);
+      break;
+    case 32: // space
+      Tetris.Block.move(0, 0, -1);
+      break;
+
+    case 87: // up (w)
+      Tetris.Block.rotate(90, 0, 0);
+      break;
+    case 83: // down (s)
+      Tetris.Block.rotate(-90, 0, 0);
+      break;
+
+    case 65: // left(a)
+      Tetris.Block.rotate(0, 0, 90);
+      break;
+    case 68: // right (d)
+      Tetris.Block.rotate(0, 0, -90);
+      break;
+
+    case 81: // (q)
+      Tetris.Block.rotate(0, 90, 0);
+      break;
+    case 69: // (e)
+      Tetris.Block.rotate(0, -90, 0);
+      break;
+  }
+}, false);
--- public/javascripts/tetris.js
+++ public/javascripts/tetris.js
@@ -1,15 +1,3 @@
-if ( !window.requestAnimationFrame ) {
-  window.requestAnimationFrame = ( function() {
-    return window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      window.oRequestAnimationFrame ||
-      window.msRequestAnimationFrame ||
-      function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
-	window.setTimeout( callback, 1000 / 60 );
-      };
-  })();
-}
-
 var Tetris = {};
 
 Tetris.init = function() {
@@ -27,13 +15,16 @@ Tetris.init = function() {
   // and a scene
   Tetris.renderer = new THREE.WebGLRenderer();
   Tetris.camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
-	                                        ASPECT,
-	                                        NEAR,
-	                                        FAR  );
+                                                ASPECT,
+                                                NEAR,
+                                                FAR  );
   Tetris.scene = new THREE.Scene();
 
   // the camera starts at 0,0,0 so pull it back
   Tetris.camera.position.z = 600;
+  console.log(Tetris.camera.position.x);
+  console.log(Tetris.camera.position.y);
+  console.log(Tetris.camera.position.z);
   Tetris.scene.add(Tetris.camera);
 
   // start the renderer
@@ -42,6 +33,7 @@ Tetris.init = function() {
   // attach the render-supplied DOM element
   document.body.appendChild(Tetris.renderer.domElement);
 
+  // to be continued...
   // configuration object
   var boundingBoxConfig = {
     width: 360,
@@ -51,18 +43,25 @@ Tetris.init = function() {
     splitY: 6,
     splitZ: 20
   };
+
   Tetris.boundingBoxConfig = boundingBoxConfig;
   Tetris.blockSize = boundingBoxConfig.width/boundingBoxConfig.splitX;
 
   var boundingBox = new THREE.Mesh(
-    new THREE.CubeGeometry(boundingBoxConfig.width, boundingBoxConfig.height, boundingBoxConfig.depth, boundingBoxConfig.splitX, boundingBoxConfig.splitY, boundingBoxConfig.splitZ),
+    new THREE.CubeGeometry(
+      boundingBoxConfig.width, boundingBoxConfig.height, boundingBoxConfig.depth,
+      boundingBoxConfig.splitX, boundingBoxConfig.splitY, boundingBoxConfig.splitZ),
     new THREE.MeshBasicMaterial( { color: 0xffaa00, wireframe: true } )
   );
-//  boundingBox.position.z = 0;
+  console.log(boundingBox.position.x);
+  console.log(boundingBox.position.y);
+  console.log(boundingBox.position.z);
+
   Tetris.scene.add(boundingBox);
 
+  // first render
   Tetris.renderer.render(Tetris.scene, Tetris.camera);
-
+  // to be continued...
   Tetris.stats = new Stats();
   Tetris.stats.domElement.style.position = 'absolute';
   Tetris.stats.domElement.style.top = '10px';
@@ -74,15 +73,25 @@ Tetris.init = function() {
     Tetris.start();
   });
 };
-
 Tetris.start = function() {
   document.getElementById("menu").style.display = "none";
   Tetris.pointsDOM = document.getElementById("points");
   Tetris.pointsDOM.style.display = "block";
 
   Tetris.animate();
+  Tetris.Block.generate();
 };
-
+if ( !window.requestAnimationFrame ) {
+  window.requestAnimationFrame = ( function() {
+                                     return window.webkitRequestAnimationFrame ||
+                                       window.mozRequestAnimationFrame ||
+                                       window.oRequestAnimationFrame ||
+                                       window.msRequestAnimationFrame ||
+                                       function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+                                         window.setTimeout( callback, 1000 / 60 );
+                                       };
+                                   })();
+}
 Tetris.gameStepTime = 1000;
 
 Tetris.frameTime = 0; // ms
@@ -90,7 +99,6 @@ Tetris.cumulatedFrameTime = 0; // ms
 Tetris._lastFrameTime = Date.now(); // timestamp
 
 Tetris.gameOver = false;
-
 Tetris.animate = function() {
   var time = Date.now();
   Tetris.frameTime = time - Tetris._lastFrameTime;
@@ -108,16 +116,13 @@ Tetris.animate = function() {
 
   if(!Tetris.gameOver) window.requestAnimationFrame(Tetris.animate);
 }
-
-
-
-// nice test:
-// var i = 0, j = 0, k = 0, interval = setInterval(function() {if(i==6) {i=0;j++;} if(j==6) {j=0;k++;} if(k==6) {clearInterval(interval); return;} Tetris.addStaticBlock(i,j,k); i++;},30)
+window.addEventListener("load", Tetris.init);
 
 Tetris.staticBlocks = [];
 Tetris.zColors = [
   0x6666ff, 0x66ffff, 0xcc68EE, 0x666633, 0x66ff66, 0x9966ff, 0x00ff66, 0x66EE33, 0x003399, 0x330099, 0xFFA500, 0x99ff00, 0xee1289, 0x71C671, 0x00BFFF, 0x666633, 0x669966, 0x9966ff
 ];
+
 Tetris.addStaticBlock = function(x,y,z) {
   if(Tetris.staticBlocks[x] === undefined) Tetris.staticBlocks[x] = [];
   if(Tetris.staticBlocks[x][y] === undefined) Tetris.staticBlocks[x][y] = [];
@@ -130,18 +135,14 @@ Tetris.addStaticBlock = function(x,y,z) {
   mesh.position.x = (x - Tetris.boundingBoxConfig.splitX/2)*Tetris.blockSize + Tetris.blockSize/2;
   mesh.position.y = (y - Tetris.boundingBoxConfig.splitY/2)*Tetris.blockSize + Tetris.blockSize/2;
   mesh.position.z = (z - Tetris.boundingBoxConfig.splitZ/2)*Tetris.blockSize + Tetris.blockSize/2;
+  mesh.overdraw = true;
 
   Tetris.scene.add(mesh);
   Tetris.staticBlocks[x][y][z] = mesh;
 };
-
 Tetris.currentPoints = 0;
 Tetris.addPoints = function(n) {
   Tetris.currentPoints += n;
   Tetris.pointsDOM.innerHTML = Tetris.currentPoints;
   Cufon.replace('#points');
 }
-
-//var i = 0, j = 0, k = 0, interval = setInterval(function() {if(i==6) {i=0;j++;} if(j==6) {j=0;k++;} if(k==6) {clearInterval(interval); return;} Tetris.addStaticBlock(i,j,k); i++;},30);
-
-window.addEventListener("load", Tetris.init);
